60aba36ab181b2db49d1438d7ab55e682edc9a76
jansaidashaik1995 <88392865+jansaidashaik1995@users.noreply.github.com>
2022-08-23 00:49:31 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'demo-wordpress');
+define('DB_NAME', 'test');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'test');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'jan@9010140931');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ces-wp-test.mysql.database.azure.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
